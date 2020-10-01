2ed468be323dbcd57e49344f38ca31fabbcd1017
suhaibwx <72232027+suhaibwx@users.noreply.github.com>
2020-10-02 06:22:32 +0800

    Add files via upload

---
+++ form1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form name="registeration" action="page2.php" method="get">
+	    <h2>User Input Form</h2>
+	    <fieldset>
+	    	<legend> Personal Particular</legend>
+	    	<label>Title</label>
+	    	<input type="radio" name="title" value="Mr"> Mr
+	    	<input type="radio" name="title" value="Mr"> Mrs
+	    	<input type="radio" name="title" value="Mr"> Ms
+	    	<input type="radio" name="title" value="Mr"> Dr
+	    	<input type="radio" name="title" value="Mr"> Dato
+	    	<input type="radio" name="title" value="Mr"> Datin
+	    	<br>
+	    	<label>Name</label>
+	    	<input type="text" name="name">
+	        <br>
+	        <label>Year of Birth</label>
+	        <input type="text" name="yob">
+	        <br>
+
+	    	<input type="submit" value="submit">
+	    <input type="reset" value="reset">
+
+		
+	</form>
+
+</body>
+</html>
+
+	    
